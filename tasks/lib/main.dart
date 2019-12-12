58267a36d56d755301fffb1d0c63170746a3c0a5
Anand-S23 <42609735+Anand-S23@users.noreply.github.com>
2019-12-11 19:54:25 -0500

    making basic nav bar

--- tasks/lib/main.dart
+++ tasks/lib/main.dart
@@ -3,19 +3,74 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  static const String _title = 'Tasks';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tasks',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text('Tasks'),
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Calender',
+      style: optionStyle,
+    ),
+    Text(
+      'Settings',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tasks'),
+        backgroundColor: Colors.purple[800],
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
           ),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calender),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.purple[800],
+        onTap: _onItemTapped,
       ),
     );
   }
